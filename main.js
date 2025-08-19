d321eace411389b46155c17db28dc781112c77f1
Nikhil-dev-wq <nikhilkar181@gmail.com>
2025-08-19 22:32:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { io } from "https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.8.1/socket.io.esm.min.js";
-const socket = io("http://localhost:5000");
+const socket = io("https://chat-backend-mmf7.onrender.com");
 
 // ---------------- DOM ELEMENTS ----------------
 const roomIdInput = document.getElementById("room-id-input");
